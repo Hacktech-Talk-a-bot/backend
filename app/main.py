6eaa46b23f068c15fbf1f2939fde05c52fdb8459
Adrian Padurean <securiteradmin@gmail.com>
2024-10-27 11:44:39 +0200

    intermid

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ from app.api.endpoints import router
 # from app.api.v1.endpoints import prototype  # Add sections import
 from app.core.config import settings
 from app.core.database import create_database
+from app.llm.stream_router import stream_router
 from app.llm.survey_router import survey_router
 
 create_database()
@@ -23,9 +24,12 @@ load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 logger = logging.getLogger(__name__)
 
+
+
 app = FastAPI()
 app.include_router(survey_router, prefix=settings.API_V1_STR, tags=["survey"])
 app.include_router(router, prefix=settings.API_V1_STR, tags=["forms"])
+app.include_router(stream_router, prefix=settings.API_V1_STR, tags=["forms"])
 
 OPENAI_URL = "https://api.openai.com/v1/chat/completions"
 client = OpenAI()
